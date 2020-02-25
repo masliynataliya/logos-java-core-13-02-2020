b715d18cb27f7bb337576454aef28bbcf3c7771e
Alboino <alboinok@gmail.com>
2020-02-25 21:33:48 +0200

    hello from Nazar

--- src/java/Nazzario/Coronavirus.java
+++
@@ -1,7 +0,0 @@
-package Nazzario;
-
-public class Coronavirus {
-    public static void main(String[] args) {
-        System.out.println("Hello world, you'll die");
-    }
-}
---
+++ src/java/javacore/lesson02/homework/nazzario/Coronavirus.java
@@ -0,0 +1,7 @@
+package javacore.lesson02.homework.nazzario;
+
+public class Coronavirus {
+    public static void main(String[] args) {
+        System.out.println("Hello world, you'll die :D");
+    }
+}
