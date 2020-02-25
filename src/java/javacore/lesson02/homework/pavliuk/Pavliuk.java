2329702dfb6acc16c68988a2080d71094d2d6ac4
PavliukMaks <maksym0416@gmail.com>
2020-02-25 21:26:38 +0200

    Pavliuk 1 commit

--- src/java/javacore/lesson02/homework/pavliuk/Pavliuk.java
+++ src/java/javacore/lesson02/homework/pavliuk/Pavliuk.java
@@ -1,5 +1,9 @@
 package javacore.lesson02.homework.pavliuk;
 
 public class Pavliuk {
+        public static void main(String[] args) {
+            System.out.println("Hello world");
+        }
+    }
 
-}
+
