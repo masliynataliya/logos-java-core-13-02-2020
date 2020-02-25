a48fd208624b905ac2e08b95da27bd6bd946d899
PavliukMaks <maksym0416@gmail.com>
2020-02-25 21:31:32 +0200

    Corona is alive

--- src/java/Nazzario/Coronavirus.java
+++ src/java/Nazzario/Coronavirus.java
@@ -2,6 +2,6 @@ package Nazzario;
 
 public class Coronavirus {
     public static void main(String[] args) {
-        System.out.println("Hello world, you'll die");
+        System.out.println("Hello world, you'll die,Thank you");
     }
 }
