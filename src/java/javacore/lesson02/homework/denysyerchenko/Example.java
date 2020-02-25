e9c56900189160323bde84a494d933c580a451fd
Ivanna <ivanna.skipar@gmail.com>
2020-02-25 21:12:23 +0200

    Denys Yerchenko first commit

--- src/java/javacore/lesson02/homework/denysyerchenko/Example.java
+++ src/java/javacore/lesson02/homework/denysyerchenko/Example.java
@@ -1,7 +1,9 @@
 package javacore.lesson02.homework.denysyerchenko;
 
 public class Example {
+    public static void main(String[] args) {
 
 
-    System.out.print("Hello")
+        System.out.print("Hello");
+    }
 }
