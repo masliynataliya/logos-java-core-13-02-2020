72266afe1476f23d148d8e2b05c4bd475eb612c2
Igor Payenskii <38349979+Lvivsky@users.noreply.github.com>
2020-02-25 21:52:41 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/java/javacore/lesson02/homework/Nazzario/Coronavirus.java

--- src/java/javacore/lesson02/homework/ihorpayensky/app.java
+++ src/java/javacore/lesson02/homework/ihorpayensky/app.java
@@ -2,6 +2,6 @@ package javacore.lesson02.homework.ihorpayensky;
 
 public class app {
     public static void main(String[] args) {
-        System.out.println("Hellho!");
+        System.out.println("Hello!");
     }
 }
