9a68e890fc49b6a8f451f1d1a2258462306dfadc
Alboino <alboinok@gmail.com>
2020-02-25 21:44:15 +0200

    Better than ver

--- src/java/javacore/lesson02/homework/katerynakikidzhan/Example1.java
+++ src/java/javacore/lesson02/homework/katerynakikidzhan/Example1.java
@@ -2,6 +2,6 @@ package javacore.lesson02.homework.katerynakikidzhan;
 
 public class Example1 {
     public static void main(String[] args) {
-        System.out.println("1");
+        System.out.println("1 shot,one hit");
     }
 }
