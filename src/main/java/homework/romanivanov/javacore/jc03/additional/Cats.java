43a80676427d6ba657f52515767cc986680c273b
Eyjafjallajokull-Roman <swany500@gmail.com>
2020-02-28 20:46:02 +0200

    Homework jc03 + additional

---
+++ src/main/java/homework/romanivanov/javacore/jc03/additional/Cats.java
@@ -0,0 +1,40 @@
+package homework.romanivanov.javacore.jc03.additional;
+
+public class Cats {
+    private int hp = 1000;
+    private   String name;
+    private int strength;
+    private int  age;
+    private int luck;
+    private int x = 0;
+    private int z = 0;
+
+    public Cats (String name, int strength, int age,int luck){
+        this.name = name;
+        this.strength = strength;
+        this.age = age;
+        this.luck = luck;
+    }
+    public void fightCat(Cats anotherCat){
+        while (hp>=1000) {
+            int a = (int) (Math.random() * this.luck) + (this.strength - this.age);
+            int b = (int) (Math.random() * anotherCat.luck) + (anotherCat.strength - anotherCat.age);
+            x = a + x;
+            z = b + z;
+            if (x > hp) {
+                System.out.println("Первый кот победил");
+                break;
+
+            }
+            if (z > hp) {
+                System.out.println("Второй кот победи");
+                break;
+
+            }
+
+        }
+
+    }
+
+
+}
