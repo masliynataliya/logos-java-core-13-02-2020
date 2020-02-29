df901162c5b433c032f98eae31e1546cdddd2b43
DenysYerchenko <denys.yerchenko@gmail.com>
2020-02-29 09:49:26 +0200

    add fishes hw

--- src/main/java/homework/denysyerchenko/fishes/ApplicationFish.java
+++ src/main/java/homework/denysyerchenko/fishes/ApplicationFish.java
@@ -4,7 +4,7 @@ public class ApplicationFish {
     public static void main(String[] args) {
         Fish fish1 = new Fish("Cleo", "green",3);
         Fish fish2 = new Fish("Nemo",5);
-        Fish fish3 = new Fish(4, "gold");
+        Fish fish3 = new Fish(2, "gold");
         Fish fish4 = new Fish("Larry");
         Fish fish5 = new Fish(){};
 
