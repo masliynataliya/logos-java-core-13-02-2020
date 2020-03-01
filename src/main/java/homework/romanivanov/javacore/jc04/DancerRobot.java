fce835d45bb84f61049f5f8067b5f5502b80e345
Eyjafjallajokull-Roman <swany500@gmail.com>
2020-03-01 13:53:36 +0200

    jc04 Robot

---
+++ src/main/java/homework/romanivanov/javacore/jc04/DancerRobot.java
@@ -0,0 +1,7 @@
+package homework.romanivanov.javacore.jc04;
+
+public class DancerRobot extends Robot {
+    public void work(){
+        System.out.println("Я DancerRobot - я просто танцюю");
+    }
+}
---
+++ src/main/java/homework/romanivanov/javacore/jc04/Robot.java
@@ -0,0 +1,12 @@
+package homework.romanivanov.javacore.jc04;
+
+public class Robot {
+
+
+
+    public void work(){
+        System.out.println("Я Robot - я просто працюю");
+    }
+
+
+}
