85288320686246d39124eddda7055832bea0df5d
DidukhIgor <didukhigor11@gmail.com>
2020-03-04 19:19:16 +0200

    javacore3

--- src/main/java/homework/didukhigor/homework/javacore3/ApplicationRectangle.java
+++ src/main/java/homework/didukhigor/homework/javacore3/ApplicationRectangle.java
@@ -4,15 +4,14 @@ public class ApplicationRectangle {
     public static void main(String[] args) {
 
         Rectangle myRectangle = new Rectangle();
+        Rectangle myRectangle2 = new Rectangle(24,34);
 
         double perimeter = myRectangle.perimeter();
         double area = myRectangle.area();
 
-        Rectangle myRectangle2 = new Rectangle();
+        System.out.println("Периметр прямокутника = " + perimeter + " , Площа прямокутника = " + area);
 
-        System.out.println("Периметр прямокутника = "+ perimeter + " , Площа прямокутника = "+ area);
-        System.out.println("Периметр прямокутника = "+ perimeter + " , Площа прямокутника = "+ area);
-
+        }
     }
-}
+
 
--- src/main/java/homework/didukhigor/homework/javacore3/Rectangle.java
+++ src/main/java/homework/didukhigor/homework/javacore3/Rectangle.java
@@ -1,30 +1,29 @@
 
 package homework.didukhigor.homework.javacore3;
 
-import java.security.PublicKey;
-
 public class Rectangle {
 
-        double length;
-        double width;
-
-        public Rectangle(double length, double width){
-            this.length = length;
-            this.width = width;
-        }
-
-        public Rectangle(){
-            this.length = 18;
-            this.width = 20;
-        }
-
-        double perimeter(){
-            return (length*2)+(width*2);
-        }
-        double area(){
-            return length*width;
-        }
+    double length;
+    double width;
 
 
+    public  Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
     }
+
+    public  Rectangle() {
+        this.length = 18;
+        this.width = 20;
+    }
+
+    double perimeter() {
+        return (length * 2) + (width * 2);
+    }
+
+    double area() {
+        return length * width; }
+
+}
+
 
