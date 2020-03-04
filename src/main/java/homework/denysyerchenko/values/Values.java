cd8e3d71f67d286b0fe2df5be8c86b8192a9763a
DenysYerchenko <denys.yerchenko@gmail.com>
2020-03-04 17:32:21 +0200

    add values hw

---
+++ src/main/java/homework/denysyerchenko/values/Values.java
@@ -0,0 +1,10 @@
+package homework.denysyerchenko.values;
+
+public class Values {
+    public static void main(String[] args) {
+
+        System.out.println("The max value of the float = " + Math.getExponent(Float.MAX_VALUE) + " The min value of the float = " + Math.getExponent(Float.MIN_VALUE));
+        System.out.println("The max value of the double = " + Math.getExponent(Double.MAX_VALUE) + " The min value of the double = " + Math.getExponent(Double.MIN_VALUE));
+
+    }
+}
