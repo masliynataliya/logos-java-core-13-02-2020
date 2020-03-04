3acb2f158dc2f675b34e9bbedf7ae35e3c2b0640
DidukhIgor <didukhigor11@gmail.com>
2020-03-04 21:20:08 +0200

    javacore3

--- src/main/java/homework/didukhigor/homework/javacore3/ApplicationRectangle.java
+++ src/main/java/homework/didukhigor/homework/javacore3/ApplicationRectangle.java
@@ -4,12 +4,16 @@ public class ApplicationRectangle {
     public static void main(String[] args) {
 
         Rectangle myRectangle = new Rectangle();
-        Rectangle myRectangle2 = new Rectangle(24,34);
+        Rectangle myRectangle2 = new Rectangle(2,6);
 
         double perimeter = myRectangle.perimeter();
         double area = myRectangle.area();
 
+        double perimeter2 = myRectangle2.perimeter2();
+        double area2 = myRectangle2.area2();
+
         System.out.println("Периметр прямокутника = " + perimeter + " , Площа прямокутника = " + area);
+        System.out.println("Периметр прямокутника = " + perimeter2 + " , Площа прямокутника = " + area2);
 
         }
     }
--- src/main/java/homework/didukhigor/homework/javacore3/Rectangle.java
+++ src/main/java/homework/didukhigor/homework/javacore3/Rectangle.java
@@ -6,23 +6,30 @@ public class Rectangle {
     double length;
     double width;
 
-
-    public  Rectangle(double length, double width) {
+    public Rectangle(double length,double width) {
         this.length = length;
         this.width = width;
     }
 
-    public  Rectangle() {
-        this.length = 18;
-        this.width = 20;
+    public Rectangle() {
+        this.length = 15;
+        this.width = 30;
     }
+
 
     double perimeter() {
         return (length * 2) + (width * 2);
     }
+    double perimeter2() {
+        return (length * 2) + (width * 2);
+    }
 
     double area() {
+        return length * width;
+    }
+    double area2() {
         return length * width; }
+
 
 }
 
