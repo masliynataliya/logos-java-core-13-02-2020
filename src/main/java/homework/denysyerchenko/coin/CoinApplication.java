91e5b9e437fa266fc61ec9e475a4610020ff05af
DenysYerchenko <denys.yerchenko@gmail.com>
2020-03-04 17:04:07 +0200

    add coin hw

---
+++ src/main/java/homework/denysyerchenko/coin/CoinApplication.java
@@ -0,0 +1,43 @@
+package homework.denysyerchenko.coin;
+
+public class CoinApplication {
+    public static void main(String[] args) {
+
+        Coin coin = new Coin();
+
+        System.out.println(coin.flipACoin());
+
+        System.out.println(coin.flipACoin());
+
+        System.out.println(coin.flipACoin());
+
+        System.out.println(coin.flipACoin());
+
+        System.out.println(coin.flipACoin());
+
+        System.out.println(coin.flipACoin());
+
+        System.out.println(coin.flipACoin());
+
+        System.out.println(coin.flipACoin());
+
+        System.out.println(coin.flipACoin());
+
+        System.out.println(coin.flipACoin());
+
+        System.out.println(coin.flipACoin());
+
+        System.out.println(coin.flipACoin());
+
+        System.out.println(coin.flipACoin());
+
+
+
+
+
+
+
+
+
+    }
+}
