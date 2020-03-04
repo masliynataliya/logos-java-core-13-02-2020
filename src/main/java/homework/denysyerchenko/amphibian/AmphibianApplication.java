a6db65237f1e51102fb66c974f127d0f724ee7d2
DenysYerchenko <denys.yerchenko@gmail.com>
2020-03-04 20:02:04 +0200

    add amphibian hw

---
+++ src/main/java/homework/denysyerchenko/amphibian/AmphibianApplication.java
@@ -0,0 +1,13 @@
+package homework.denysyerchenko.amphibian;
+
+public class AmphibianApplication {
+    public static void main(String[] args) {
+        Frog frog = new Frog();
+
+        frog.eat();
+        frog.sleep();
+        frog.swim();
+        frog.walk();
+
+    }
+}
