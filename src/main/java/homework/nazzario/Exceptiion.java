18e7ad65da1dabcdd146aa96d33db4093530429d
Alboino <alboinok@gmail.com>
2020-03-12 21:40:48 +0200

    ВруумВрууум

---
+++ src/main/java/homework/nazzario/Exceptiion.java
@@ -0,0 +1,13 @@
+package homework.nazzario;
+
+
+import java.util.Scanner;
+
+public class Exceptiion {
+    public static void main(String[] args) {
+        System.out.println("Введіть число " );
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+
+    }
+}
