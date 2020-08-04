2c1de3355a477784f44eccb0b751d04813d38921
yabluko <61472883+yabluko@users.noreply.github.com>
2020-08-04 21:58:02 +0300

    part-work-lesson11-12

---
+++ src/main/java/homework/yuriirobak/classwork/CallsBook.java
@@ -0,0 +1,20 @@
+package homework.yuriirobak.classwork;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CallsBook {
+
+  public static void main(String[] args){
+    List knuga  = new ArrayList();
+
+
+
+    System.out.println("Щоб подивитись телефону книгу натисність 1 ");
+
+
+
+
+
+  }
+}
--- src/main/java/homework/yuriirobak/main.java
+++ src/main/java/homework/yuriirobak/main.java
@@ -3,6 +3,10 @@ package homework.yuriirobak;
 public class main {
     public static void main(String[] args) {
 
+
+
+
+
         System.out.println("hell");
     }
 }
