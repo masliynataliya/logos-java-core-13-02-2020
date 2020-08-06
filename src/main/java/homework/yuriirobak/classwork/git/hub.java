13d2be138c99b2fb9a44bc20af7c25fbe73a8816
yabluko <61472883+yabluko@users.noreply.github.com>
2020-08-06 19:16:16 +0300

    part-work-lesson11-12

---
+++ src/main/java/homework/yuriirobak/classwork/git/hub.java
@@ -0,0 +1,16 @@
+package homework.yuriirobak.classwork.git;
+
+import java.util.Scanner;
+
+public class hub {
+
+
+        public static void main(String[] args) {
+
+            Scanner scanner = new Scanner()
+
+
+            System.out.println(s);
+
+    }
+}
