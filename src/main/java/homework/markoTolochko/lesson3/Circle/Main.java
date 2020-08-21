f656c72f43a733c05d36e75aaf9345e73cac2be1
markotolochko <t.marko.tolochko@gmail.com>
2020-08-21 19:07:16 +0300

    lesson11 IntegerArraySorting

---
+++ src/main/java/homework/markoTolochko/lesson3/Circle/Main.java
@@ -0,0 +1,11 @@
+package homework.markoTolochko.lesson3.Circle;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Circle circle = new Circle(15, 30);
+        circle.circleLenght();
+        circle.circleSquare();
+
+    }
+}
