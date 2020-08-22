1b9b22ce698cb29cc803f72b37879ea98523d61e
markotolochko <t.marko.tolochko@gmail.com>
2020-08-22 13:31:24 +0300

    lesson11 IntegerArraySorting

---
+++ src/main/java/homework/markoTolochko/lesson12CarArray/Main.java
@@ -0,0 +1,16 @@
+package homework.markoTolochko.lesson12CarArray;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        ArrayList<Car> cars = new ArrayList<>();
+        for (int i = 0; i < random.nextInt(3); i++){
+            cars.add(new Car());
+        }
+        System.out.println(cars);
+    }
+}
---
+++ src/main/java/homework/markoTolochko/lesson12CarArray/SteeringWheel.java
@@ -0,0 +1,45 @@
+package homework.markoTolochko.lesson12CarArray;
+
+import java.util.Random;
+
+public class SteeringWheel {
+    int diameter;
+    String material;
+
+    public SteeringWheel() {
+        Random random = new Random();
+        String[] materials = { "leather", "alcantara"};
+
+        this.diameter = random.nextInt(50);
+        this.material = materials[random.nextInt(materials.length)];
+    }
+
+    public SteeringWheel(int diameter, String material) {
+        this.diameter = diameter;
+        this.material = material;
+    }
+
+    @Override
+    public String toString() {
+        return "SteeringWheel{" +
+                "diameter=" + diameter +
+                ", material='" + material + '\'' +
+                '}';
+    }
+
+    public int getDiameter() {
+        return diameter;
+    }
+
+    public void setDiameter(int diameter) {
+        this.diameter = diameter;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+}
