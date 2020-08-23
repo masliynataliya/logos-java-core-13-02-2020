2fbd51ddc413163ca4053db0a354e02c0949dd58
markotolochko <t.marko.tolochko@gmail.com>
2020-08-23 17:21:01 +0300

    lesson11 IntegerArraySorting

--- src/main/java/homework/markoTolochko/lesson15MapZooClub/MainMain.java
+++ src/main/java/homework/markoTolochko/lesson15MapZooClub/MainMain.java
@@ -2,12 +2,9 @@ package homework.markoTolochko.lesson15MapZooClub;
 
 import java.util.*;
 
-public class Main2 {
-
-
+public class MainMain {
     public static void main(String[] args) {
-        Map <Person, List<Animal>> map = new HashMap<>();
-        List<Animal> listAnimal = new ArrayList<>();
+        HashMap<Person, List<Animal>> mapGeneral = new HashMap<>();
 
         while (true) {
             System.out.println("add person to club -- 1");
@@ -20,39 +17,37 @@ public class Main2 {
             Scanner scan = new Scanner(System.in);
             int user = scan.nextInt();
 
-            switch (user){
+            switch (user) {
                 case 1:
                     System.out.println("enter name");
                     String namePerson = scan.next();
                     System.out.println("enter age ");
                     Integer agePerson = scan.nextInt();
-                    map.put(new Person(namePerson,agePerson), new ArrayList<>());
+                    mapGeneral.put(new Person(namePerson, agePerson), new ArrayList<>());
                     break;
                 case 2:
                     System.out.println("enter name who will get pet");
                     String nameGuyWithAnimal = scan.next();
                     System.out.println("enter age that guy ");
                     Integer ageGuyWithAnimal = scan.nextInt();
-
                     System.out.println("enter animal type ");
                     String animalTypo = scan.next();
                     System.out.println("enter animal name ");
                     String animalName = scan.next();
-                    Person person = new Person(nameGuyWithAnimal, ageGuyWithAnimal);
-                   // Animal animal = new Animal(animalTypo, animalName);
-
-                    //map.put(new Person(nameGuyWithAnimal, ageGuyWithAnimal), (List<Animal>) new Animal(animalTypo, animalName));
-                    map.get(person).add(new Animal(animalTypo, animalName));
+                    Person person = new Person();
+                    Animal animal = new Animal();
+                    mapGeneral.get(new Person(nameGuyWithAnimal, ageGuyWithAnimal)).add(new Animal(animalTypo, animalName));
                     break;
                 case 3:
-                    ZooClub zooClub = new ZooClub(map);
-                    System.out.println(zooClub);
-
+                    //show list
+                    Set<Map.Entry<Person, List<Animal>>> entries = mapGeneral.entrySet();
+                    Iterator<Map.Entry<Person, List<Animal>>> iterator = entries.iterator();
+                    while (iterator.hasNext()){
+                        System.out.println(iterator.next());
+                    }
 
             }
-
-
-
         }
     }
+
 }
